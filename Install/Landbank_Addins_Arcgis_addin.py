1176331425bda7435f7f7c0fd4bef51a995283d8
dalimunthe <rasyid.dalimunthe@gmail.com>
2017-10-10 09:54:31 +0700

    new addins

---
+++ Install/Landbank_Addins_Arcgis_addin.py
@@ -0,0 +1,18 @@
+import arcpy
+import pythonaddins
+
+class ButtonClass1(object):
+    """Implementation for Landbank_Addins_Arcgis_addin.button (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        pass
+
+class ButtonClass2(object):
+    """Implementation for Landbank_Addins_Arcgis_addin.button_1 (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        pass
